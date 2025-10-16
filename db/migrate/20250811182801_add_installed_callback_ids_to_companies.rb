575ac7c8c74d9130df79236d7592fa23810a15b8
Ale Lopez <alelopez.utnfrt@gmail.com>
2025-10-16 16:47:01 -0300

    Rubocop fix

--- db/migrate/20250811182801_add_installed_callback_ids_to_companies.rb
+++ db/migrate/20250811182801_add_installed_callback_ids_to_companies.rb
@@ -1,5 +1,6 @@
 class AddInstalledCallbackIdsToCompanies < ActiveRecord::Migration[8.0]
   def change
-    add_column :companies, :installed_callback_ids, :jsonb, default: [] unless column_exists?(:companies, :installed_callback_ids)
+    add_column :companies, :installed_callback_ids, :jsonb, default: [] unless column_exists?(:companies,
+:installed_callback_ids)
   end
 end
