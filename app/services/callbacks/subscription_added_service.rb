9833bfc8f7644d03d100d98d79239bea045c6359
Macr0os <matiasnfsw114@gmail.com>
2025-11-28 12:53:06 -0300

    update items individually

--- app/services/callbacks/subscription_added_service.rb
+++ app/services/callbacks/subscription_added_service.rb
@@ -8,8 +8,15 @@ class Callbacks::SubscriptionAddedService < Callbacks::BaseService
     update_cart_metadata(cart_token, { "price_type" => "preferred_customer" })
 
     if cart_items.any?
-      items_data = build_subscription_items_data(cart_items)
-      update_cart_items_prices(cart_token, items_data)
+      cart_items.each do |item|
+        item_data = [ {
+          "id" => item["id"],
+          "price" => item["subscription_price"] || item["price"],
+        } ]
+        Rails.logger.info "Updating item #{item['id']} to subscription price: #{item_data.first['price']}"
+        update_cart_items_prices(cart_token, item_data)
+      end
+
       update_cart_totals(cart_token, cart_items, use_subscription_prices: true)
     end
 
