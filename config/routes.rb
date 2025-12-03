6900099b37624d50ed276cc4f6b39e901c02c176
Macr0os <matiasnfsw114@gmail.com>
2025-12-03 15:31:48 -0300

    removed unnecesary route

--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,6 @@ Rails.application.routes.draw do
   post "callback/subscription_added", to: "callbacks/subscription_added#create"
   post "callback/subscription_removed", to: "callbacks/subscription_removed#create"
   post "callback/update_cart_email", to: "callbacks/update_cart_email#create"
-  post "callback/:callback_name", to: "callbacks#create", as: :callback
 
   namespace :admin do
     get "dashboard/index"
