51ad2111a2ac4829843f14a677bb711a1b886965
Macr0os <matiasnfsw114@gmail.com>
2025-12-03 12:07:57 -0300

    Added params permit for subscription added and removed

--- app/controllers/callbacks/subscription_added_controller.rb
+++ app/controllers/callbacks/subscription_added_controller.rb
@@ -4,4 +4,16 @@ private
   def service_class
     Callbacks::SubscriptionAddedService
   end
+
+  def permitted_params
+    params.permit(
+      :callback_name,
+      :schema_version,
+      :schema_hash,
+      cart: {},
+      cart_item: {},
+      context: {},
+      subscription_added: {}
+    )
+  end
 end
